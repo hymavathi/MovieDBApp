94d7d1a6bf00ff08e21a776d99ae905a3441dcb9
horuganti <hymavathi.oruganti@allscripts.com>
2018-05-08 21:56:54 -0400

    Initial commit

--- app/src/main/java/com/udacity/projects/moviesdbapp/services/MovieService.java
+++ app/src/main/java/com/udacity/projects/moviesdbapp/services/MovieService.java
@@ -21,7 +21,7 @@ public class MovieService {
 
     static final String MOVIEDB_API_URL = "https://image.tmdb.org/t/p/";
 
-    static final String MOVIEDB_API_KEY = "0ed08a1b3f4360b41d7eaad139c6b94b";
+   static final String MOVIEDB_API_KEY = "APIKEY";
 
     static final String MOVIE_CONFIG_URL = "http://api.themoviedb.org/3/movie/popular";
 
